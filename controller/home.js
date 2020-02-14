2efb198d9bfbb921c9de8c432a45ddcd08f5e4b5
Thomas Soutif <th.soutif@gmail.com>
2020-02-14 19:24:41 +0100

    add the redirection to the controller from index.html (get by url)

---
+++ controller/home.js
@@ -0,0 +1,13 @@
+function Home_Controller()
+{
+
+    console.log("i'm in home conroller");
+    //console.log(window.location.pathname);
+
+    //console.log(document.location.pathname);
+    $("<html>").load("./views/head.html");
+
+
+
+
+}
