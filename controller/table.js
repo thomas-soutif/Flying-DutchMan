1d0284ca0a8c43a99003ab40ad0373957f26c0f5
Thomas Soutif <th.soutif@gmail.com>
2020-02-26 19:22:43 +0100

    Integrate the table selection interface

--- controller/table.js
+++ controller/table.js
@@ -7,6 +7,7 @@ class Table_Controller extends Master_Controller {
     index(){ // The default function call if there is no action on the page (usually call only to load the page once
         console.log("i'm in Table controller");
         this.start_page(translate("sentence_ChooseYourTable","sw"),"table");
+        this.addHTMLFileToDOM("./views/table/table_selection.html","body");
         this.end_page();
     }
 
