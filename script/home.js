d7e70de1470ffa9a7a07195b5ee4ee7253016c7c
YoyoInUU <50628928+YoyoInUU@users.noreply.github.com>
2020-05-03 18:02:42 +0200

    Update home.js

--- script/home.js
+++ script/home.js
@@ -1,37 +1,39 @@
+/**
+ * File: home.js
+ *
+ * Show our menu on the home page.
+ *
+ * Author: Yu-Lun Chang, Thomas Soutif
+ */
+
 $(document).ready(function () {
-
-
     translateAllDOM();
     $("#menu-home").addClass("active");
-    loadAndShowMenu();
-
-
+    loadAndShowMenu();  // Load our menu.
 });
 
 
 function  loadAndShowMenu()
 {
     let response = ajaxCall("ajax_getAllMenu", null);
-    if(!response.error)
+    if(!response.error)  // Show menu if menu exist.
     {
-
         let menu = response.data.menu;
         for (let i = 0; i < menu.length; i++) {
             let menuListHTML =
                 $("<div/>").attr("data-beverageId", menu[i].article_id).attr("class","itemMenuList").append($("<span>").attr("class", "menuListNameItem").append(menu[i].allInfo.name)).append("<br/>").
                 append($("<span>").attr("class", "menuListPriceItem").append(menu[i].price +" SEK"));
-
             $("#menuList").append(menuListHTML);
         }
     }
-    else
+    else  // Deal with error.
     {
         console.log(response.errorMessage);
-        if(response.error === 1) // empty list
+        if(response.error === 1)  // Empty list.
         {
-            obj_ListMenu = new ListMenuAjax(); // the list should be create
+            obj_ListMenu = new ListMenuAjax(); // The list should be create.
         }
     }
-    translateElementInDOM("#menuList");
+    translateElementInDOM("#menuList");  // Deal with multi-language.
 
 }
